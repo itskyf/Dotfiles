4a85174371203779741994c54059ce2e6977c697
Ky Anh <40734986+itskyf@users.noreply.github.com>
2024-01-25 17:13:40 +0700

    fix[Neovim]: override shortcut when open definition in new tab

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -166,7 +166,7 @@ vim.api.nvim_create_autocmd('LspAttach', {
 		vim.bo[ev.buf].omnifunc = 'v:lua.vim.lsp.omnifunc'
 
 		local opts = { buffer = ev.buf }
-		vim.keymap.set('n', 'gt', '<cmd>tab split | lua vim.lsp.buf.definition()<CR>', opts)
+		vim.keymap.set('n', '<leader>g', '<cmd>tab split | lua vim.lsp.buf.definition()<CR>', opts)
 		vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
 		vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
 		vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
@@ -181,8 +181,6 @@ vim.api.nvim_create_autocmd('LspAttach', {
 		vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, opts)
 		vim.keymap.set({ 'n', 'v' }, '<space>ca', vim.lsp.buf.code_action, opts)
 		vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
-		vim.keymap.set('n', '<space>f', function()
-			vim.lsp.buf.format { async = true }
-		end, opts)
+		vim.keymap.set('n', '<space>f', function() vim.lsp.buf.format { async = true } end, opts)
 	end,
 })
